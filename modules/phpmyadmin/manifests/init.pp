e9ee2de6e8cd486892bbd2adad649c0ea0d25277
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-12 11:14:28 +1000

    Add disabled_extensions support

--- modules/phpmyadmin/manifests/init.pp
+++ modules/phpmyadmin/manifests/init.pp
@@ -1,18 +1,26 @@
 # A Chassis extension to install and configure phpMyAdmin on your server
 class phpmyadmin (
-  $config,
-  $path = '/vagrant/extensions/phpmyadmin',
-  $database_user     = $config[database][user],
-  $database_password = $config[database][password]
+	$config,
+	$path = '/vagrant/extensions/phpmyadmin',
+	$database_user     = $config[database][user],
+	$database_password = $config[database][password]
 ) {
-  file { "${path}/phpmyadmin/config.inc.php":
-    ensure  => file,
-    content => template('phpmyadmin/config.inc.php.erb')
-  }
+	if ( ! empty( $config[disabled_extensions] ) and 'chassis/phpmyadmin' in $config[disabled_extensions] ) {
+		$file = absent
+		$link = absent
+	} else {
+		$file = file
+		$link = link
+	}
 
-  file { '/vagrant/phpmyadmin':
-    ensure => link,
-    target => '/vagrant/extensions/phpmyadmin/phpmyadmin',
-    notify => Service['nginx'],
-  }
+	file { "${path}/phpmyadmin/config.inc.php":
+		ensure  => $file,
+		content => template('phpmyadmin/config.inc.php.erb')
+	}
+
+	file { '/vagrant/phpmyadmin':
+		ensure => $link,
+		target => '/vagrant/extensions/phpmyadmin/phpmyadmin',
+		notify => Service['nginx'],
+	}
 }
